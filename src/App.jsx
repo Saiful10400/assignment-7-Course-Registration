4c1cccfa5931c998469ca034d0b83b0e7fdb496e
saiful10400 <saifulislam10400i5@gmail.com>
2023-09-15 16:00:27 +0600

    cart section's all design done.some incremantal changes happend on cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-
+import Header from "./assets/components/header";
+import CadrSecton from "./assets/components/cards section";
 
 function App() {
   const [data, setData] = useState([]);
@@ -10,13 +11,46 @@ function App() {
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
-  console.log(data);
-  // toast
 
   return (
     <>
-      <h1 className="text-5xl">Vite + React  </h1>
+      <section className=" w-11/12 mx-auto">
+        <Header></Header>
 
+        {/* this is for body section. */}
+        <section className="flex gap-7">
+          {/* for card secton */}
+          <div className="grid grid-cols-3 w-3/4 gap-6">
+            {data.map((item) => (
+              <CadrSecton item={item}></CadrSecton>
+            ))}
+          </div>
+
+
+
+          {/* for cart section. */}
+          <div className="w-1/4 p-6 bg-white rounded-xl h-[max-content]">
+            <h1 className="text-xl text-blue-500 font-bold px-4 border-b-2 border-r-gray-400 text-center">
+              Credit Hour Remaining 7 hr
+            </h1>
+            <h1 className="text-2xl text-black font-bold text-start mt-4">
+              Course Name
+            </h1>
+
+              <ol className="my-6 list-decimal font-normal text-base text-gray-600 ml-5">
+                <li>hellow saiufl</li>
+                <li>hellow gourob.</li>
+              </ol>
+
+            <h1 className="text-base font-semibold text-gray-800 py-4 border-y-2 border-gray-400 mb-4">
+              Total Credit Hour : 13
+            </h1>
+            <h1 className="font-semibold text-base text-gray-800">
+              Total Price : 48000 USD
+            </h1>
+          </div>
+        </section>
+      </section>
     </>
   );
 }
---
+++ src/assets/components/cards section.jsx
@@ -0,0 +1,24 @@
+
+import { BiDollar } from "react-icons/bi";
+import { TbBook } from "react-icons/tb";
+const CadrSecton = ({item}) => {
+    const{title,details,price,credit,image}=item
+    return (
+        <div className="p-4 rounded-xl bg-white">
+            <img className="rounded-lg w-full h-48 object-cover" src={image} alt="" />
+            <h1 className="  my-4 text-lg font-semibold">{title}</h1>
+            <p className="text-sm font-normal text-gray-600 h-20 ">{details}</p>
+            <div className="  text-base font-medium text-gray-500 flex justify-between items-center mt-8 mb-7">
+                
+                <span className="flex items-center gap-1" ><BiDollar/>Price : {price}</span>
+                
+                <span className="flex items-center gap-1" ><TbBook/>Credit : {credit}</span>
+            </div>
+            <button className="text-white py-2 w-full bg-blue-500 rounded-lg text-lg font-semibold">Select</button>
+        </div>
+    );
+};
+
+export default CadrSecton;
+
+
